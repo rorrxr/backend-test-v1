28ac641150eae6f818f17e3afd5122417345916c
rorrxr <rorrxr@naver.com>
2025-10-22 17:22:44 +0900

    [chore] : application 계층의 메트릭 수집을 위한 build.gradle.kts 수정

--- modules/application/build.gradle.kts
+++ modules/application/build.gradle.kts
@@ -10,4 +10,6 @@ dependencies {
     implementation(projects.modules.domain)
     // Only need Spring annotations (@Service) for this module
     implementation("org.springframework:spring-context")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 }
